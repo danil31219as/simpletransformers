4ac85919a7aea36a4ccadf5924d9849520a2e5bd
danasone <57491208+danil31219as@users.noreply.github.com>
2022-01-23 21:49:51 +0400

    Update models.py

--- simpletransformers/custom_models/models.py
+++ simpletransformers/custom_models/models.py
@@ -122,7 +122,7 @@ class DebertaV2ForMultiLabelSequenceClassification(DebertaV2PreTrainedModel):
     def __init__(self, config, pos_weight=None):
         super(DebertaV2ForMultiLabelSequenceClassification, self).__init__(config)
         self.num_labels = config.num_labels
-        self.deberta = DebertaV2ForSequenceClassification(config)
+        self.deberta = DebertaV2Model(config)
         self.pooler = ContextPooler(config)
         output_dim = self.pooler.output_dim
 
