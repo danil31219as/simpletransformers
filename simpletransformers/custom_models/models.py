7496d617d2ff4f31ad707b5f44736b837680539d
danasone <57491208+danil31219as@users.noreply.github.com>
2022-01-23 21:02:03 +0400

    Update models.py

--- simpletransformers/custom_models/models.py
+++ simpletransformers/custom_models/models.py
@@ -60,6 +60,24 @@ from transformers.models.xlm_roberta.modeling_xlm_roberta import (
 )
 from transformers.activations import ACT2FN
 
+def get_mask(input, local_context):
+    if not isinstance(local_context, DropoutContext):
+        dropout = local_context
+        mask = None
+    else:
+        dropout = local_context.dropout
+        dropout *= local_context.scale
+        mask = local_context.mask if local_context.reuse_mask else None
+
+    if dropout > 0 and mask is None:
+        mask = (1 - torch.empty_like(input).bernoulli_(1 - dropout)).bool()
+
+    if isinstance(local_context, DropoutContext):
+        if local_context.mask is None:
+            local_context.mask = mask
+
+    return mask, dropout
+
 class XDropout(torch.autograd.Function):
     """Optimized dropout function to save computation and memory by using mask operation instead of multiplication."""
 
