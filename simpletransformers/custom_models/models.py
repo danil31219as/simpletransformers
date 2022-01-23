c3f405eaf3dc6c3a151474276515d6f81410c925
danasone <57491208+danil31219as@users.noreply.github.com>
2022-01-23 19:26:56 +0400

    Update models.py

--- simpletransformers/custom_models/models.py
+++ simpletransformers/custom_models/models.py
@@ -14,6 +14,8 @@ from transformers import (
     XLMPreTrainedModel,
     XLNetModel,
     XLNetPreTrainedModel,
+    DebertaV2Model,
+    DebertaV2PreTrainedModel
 )
 from transformers.modeling_utils import PreTrainedModel, SequenceSummary
 from transformers.models.albert.modeling_albert import (
@@ -66,6 +68,57 @@ class BertForMultiLabelSequenceClassification(BertPreTrainedModel):
         super(BertForMultiLabelSequenceClassification, self).__init__(config)
         self.num_labels = config.num_labels
         self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
+        self.pos_weight = pos_weight
+
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        labels=None,
+    ):
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        outputs = (logits,) + outputs[
+            2:
+        ]  # add hidden states and attention if they are here
+
+        if labels is not None:
+            loss_fct = BCEWithLogitsLoss(pos_weight=self.pos_weight)
+            labels = labels.float()
+            loss = loss_fct(
+                logits.view(-1, self.num_labels), labels.view(-1, self.num_labels)
+            )
+            outputs = (loss,) + outputs
+
+        return outputs  # (loss), logits, (hidden_states), (attentions)
+    
+class DebertaV2ForMultiLabelSequenceClassification(DebertaV2PreTrainedModel):
+    """
+    Bert model adapted for multi-label sequence classification
+    """
+
+    def __init__(self, config, pos_weight=None):
+        super(DebertaV2ForMultiLabelSequenceClassification, self).__init__(config)
+        self.num_labels = config.num_labels
+        self.bert = DebertaV2Model(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
         self.pos_weight = pos_weight
